3516f32d812f8e6eb20baf1234d5c4a5c30f0eb2
yagnik1125 <140049645+yagnik1125@users.noreply.github.com>
2025-02-06 10:52:51 +0530

    Update vedio2subtitle.py

--- pages/vedio2subtitle.py
+++ pages/vedio2subtitle.py
@@ -31,7 +31,7 @@ import re
 import traceback
 
 # Initialize the Groq client
-client = Groq(api_key="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")
+client = Groq(api_key="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")
 
 # Streamlit frontend for audio input and translation
 st.title("Subtitle Generator App")
